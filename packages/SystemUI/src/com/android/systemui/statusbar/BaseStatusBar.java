ef65c945af9f3ab46c08a497a6aaf41512f18f63
0xD34D <clark@scheffsblend.com>
2013-10-05 11:11:48 -0700

    ChaosLab: Add annotation to be used when adding new features to the lab.
    This should be used when adding completely new features to ChameleonOS,
    and not for simple things that expand upon a current feature.
    
    Change-Id: I37d7250424967d41ecfb885120627f5397b2f804
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -46,6 +46,7 @@ import android.graphics.PixelFormat;
 import android.graphics.PorterDuff;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
+import android.graphics.PixelFormat;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Handler;
