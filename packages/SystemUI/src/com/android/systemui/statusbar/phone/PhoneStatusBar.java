f2ab5c0cd019e3345819e6d2ad0820e16a7c670b
0xD34D <clark@scheffsblend.com>
2013-10-07 14:27:51 -0700

    Gesture Anywhere: Add gesture anywhere feature into the ChaOS Lab [1/2]
    When enabled a user can activate the gesture anywhere view by touching
    the designated trigger area.  If a gesture is recognized as one the user
    has created and assigned to an action, that action will be performed.
    Since this is part of the system UI and implemented as an overlay, it
    will be available and usable system wide, with the exception of the
    keyguard for obvious reasons.
    
    Change-Id: Ic47abb7531cecbdd33d344b9a32402f6ffed7163
    Signed-off-by: hany <hany.alsamman@gmail.com>
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -30,6 +30,8 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
 import android.animation.TimeInterpolator;
+import android.annotation.ChaosLab;
+import android.annotation.ChaosLab.Classification;
 import android.app.ActivityManager;
 import android.app.ActivityManagerNative;
 import android.app.Notification;
@@ -899,6 +901,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     // ================================================================================
     // Constructing the view
     // ================================================================================
+    @ChaosLab(name="GestureAnywhere", classification=Classification.CHANGE_CODE)
     protected PhoneStatusBarView makeStatusBarView() {
         final Context context = mContext;
 
@@ -1037,6 +1040,14 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         // Setup pie container if enabled
         attachPieContainer(isPieEnabled());
+
+        if (mRecreating) {
+        } else {
+            //addActiveDisplayView();
+            /* ChaosLab: GestureAnywhere - BEGIN */
+            addGestureAnywhereView();
+            /* ChaosLab: GestureAnywhere - END */
+        }
 
         // figure out which pixel-format to use for the status bar.
         mPixelFormat = PixelFormat.OPAQUE;
