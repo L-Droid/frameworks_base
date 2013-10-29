399b56ec9e8f43eaff8332c2eb1dbf69132b3333
cristianomatos <cristianobmatos@gmail.com>
2013-10-29 17:30:41 -0200

    Network stats custom colors (1/2)
    Change-Id: I3c95a70273b111c7776f395f7baa60a77cea95dd
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3194,7 +3194,13 @@ public final class Settings {
          * Frequency at which stats are updated, in milliseconds
          * @hide
          */
-        public static final String STATUS_BAR_NETWORK_STATS_UPDATE_INTERVAL = "status_bar_network_stats_update_frequency";   
+        public static final String STATUS_BAR_NETWORK_STATS_UPDATE_INTERVAL = "status_bar_network_stats_update_frequency";
+
+	/**
+         * Network stats Color style
+         * @hide
+         */
+        public static final String STATUS_BAR_NETWORK_COLOR = "status_bar_network_usage_color";   
 
         /**
          * Reminder alert on / off
