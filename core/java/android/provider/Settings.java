da0c6e0ca6bfa925175f016c786a3801021e67ab
ILWT <tsubus@lagat.org>
2014-02-07 19:13:02 +0100

    Completely integrate HFM into Settings [4/4]
    * Checks if hosts file is correct after completing boot (useful after updating ROM)
    * One less package
    
    Change-Id: I103ed99a911cbeb503bb090087a3fee274784b6e
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4942,6 +4942,12 @@ public final class Settings {
         public static final int INCOMING_CALL_STYLE_FULLSCREEN_PHOTO = 1;
 
         /**
+         * Disable ads (HFM)
+         * @hide
+         */
+        public static final String HFM_DISABLE_ADS = "hfm_disable_ads";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
