0d1d7273a7d16b5d0a1ef2278092b6f3911dc882
hany <hany.alsamman@gmail.com>
2014-02-18 19:10:40 +0000

    SmoothProgressBar: Accelerate and Interpolator
    PS1 : reformat the code
    PS2 : use android animation interpolator
    
    Signed-off-by: hany <hany.alsamman@gmail.com>
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/java/android/widget/ProgressBar.java
    
    Change-Id: If1f3560fa3d25aa1e8d63a2263992b15e2f14a7c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3309,6 +3309,13 @@ public final class Settings {
 	*/
         public static final String PROGRESSBAR_COLOR_4 = "progressbar_color_4";
 
+       /**
+	*
+	* SMOOTH PROGRESS BAR interpolator
+	* @hide
+	*/
+        public static final String PROGRESSBAR_INTERPOLATOR = "progressbar_interpolator";
+
         /**
          * Whether to show the clock in status bar
          * of the stock battery icon
