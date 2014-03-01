5e10382754ecc9d748a9f4acda027101c3f84793
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2014-03-01 18:37:06 +0100

    FWB: IME animations (1/3)
    Original from here:
    Keyboard Animation https://github.com/zst123/XuiMod/commit/975cd531a9a847769b2af306495643a55ec44aaa
    thanks to zst123 for patches
    
    Patch destiled from here:
    https://gerrit.omnirom.org/#/c/4251/37
    
    Change-Id: Ie530898e851f1ffb289a290597f534f662c08847
    
    Conflicts:
    	core/java/android/inputmethodservice/InputMethodService.java
    	core/java/android/provider/Settings.java
    	core/res/res/values/arrays_custom.xml
    	core/res/res/values/symbols_custom.xml
    
    Conflicts:
    	core/res/res/values/liquid_arrays.xml
    	core/res/res/values/liquid_symbols.xml
    
    Conflicts:
    	core/res/res/values/slim_arrays.xml
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/java/com/android/internal/util/ldroid/AwesomeAnimationHelper.java
    	core/res/res/values/slim_arrays.xml
    	core/res/res/values/slim_symbols.xml
    
    Conflicts:
    	core/java/android/inputmethodservice/InputMethodService.java
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3524,6 +3524,30 @@ public final class Settings {
         public static final String MVNO_ROAMING = "mvno_roaming";
 
         /**
+         *
+         * @hide
+         */
+        public static final String ANIMATION_IME_DURATION = "animation_ime_duration";
+
+        /**
+         *
+         * @hide
+         */
+        public static final String ANIMATION_IME_ENTER = "animation_ime_enter";
+
+        /**
+         *
+         * @hide
+         */
+        public static final String ANIMATION_IME_EXIT = "animation_ime_exit";
+
+        /**
+         *
+         * @hide
+         */
+        public static final String ANIMATION_IME_INTERPOLATOR = "animation_ime_interpolator";
+
+        /**
          * Whether to enforce quiet hours regardless of the timer.
          * @hide
          */
