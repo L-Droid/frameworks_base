dc79ef58d22d009cc5d5ad293bdc652750adfad6
Evisceration <eviscerationls@gmail.com>
2014-03-13 10:10:34 +0100

    Squashed On-The-Go mode (1/2)
    On-The-Go turns on the camera of your phone and streams it to an overlay,
    which is drawn over everything.
    this makes you feel like your display is transparent, as you see the content
    behind it.
    
    its really useful when you are using your phone while walking, thats why
    we called it "On-The-Go"
    
    allows to change transparency of the overlay via the notification.
    
    have fun!
    
    Change-Id: I3efccb5fac41a946f6cb4992e9181422980ee5e4
    
    Conflicts:
    
    	core/java/com/android/internal/util/cm/QSConstants.java
    	packages/SystemUI/res/values/cr_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
    	policy/src/com/android/internal/policy/impl/GlobalActions.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -47,6 +47,7 @@ import static com.android.internal.util.cm.QSConstants.TILE_WIFI;
 import static com.android.internal.util.cm.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.cm.QSConstants.TILE_WIMAX;
 import static com.android.internal.util.cm.QSConstants.TILE_POWER;
+import static com.android.internal.util.cm.QSConstants.TILE_ONTHEGO;
 
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
@@ -100,13 +101,14 @@ import com.android.systemui.quicksettings.RemoteDisplayTile;
 import com.android.systemui.quicksettings.WiFiTile;
 import com.android.systemui.quicksettings.WifiAPTile;
 import com.android.systemui.quicksettings.PowerMenuTile; 
+import com.android.systemui.quicksettings.OnTheGoTile;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
 public class QuickSettingsController {
-    private static String TAG = "QuickSettingsController";
+    private static final String TAG = "QuickSettingsController";
 
     // Stores the broadcast receivers and content observers
     // quick tiles register for.
@@ -296,6 +298,8 @@ public class QuickSettingsController {
                 }
 	    } else if (tile.equals(TILE_POWER)) {
                 qs = new PowerMenuTile(mContext, this);     
+            } else if (tile.equals(TILE_ONTHEGO)) {
+                qs = new OnTheGoTile(mContext, this);     
             }
 
             if (qs != null) {
@@ -469,7 +473,7 @@ public class QuickSettingsController {
                 }
             }
         }
-    };
+    }
 
     void setBar(PanelBar bar) {
         mBar = bar;
