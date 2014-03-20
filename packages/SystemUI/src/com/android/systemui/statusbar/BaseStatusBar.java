9c0a136b6f05bf35d7a0fe71323a006116237d28
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2014-03-20 20:48:04 +0700

    [1/2] Base: implement App circle sidebar
    add whitelist app to include into circle view
    rebased
    make standalone patch
    
    PS.15 : rebased
    PS.16 : cleanup
    
    Change-Id: I08d2a3a7e0f8df20e801e585d4cac8547a740a3e

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -91,6 +91,7 @@ import com.android.systemui.statusbar.phone.Ticker;
 import com.android.systemui.statusbar.phone.KeyguardTouchDelegate;
 import com.android.systemui.statusbar.phone.PhoneStatusBar;
 import com.android.systemui.statusbar.policy.NotificationRowLayout;
+import com.android.systemui.statusbar.appcirclesidebar.AppCircleSidebar;
 import java.util.ArrayList;
 import java.util.Locale;
 
@@ -194,6 +195,8 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     @ChaosLab(name="GestureAnywhere", classification=Classification.NEW_FIELD)
     protected GestureAnywhereView mGestureAnywhereView;
+
+    protected AppCircleSidebar mAppCircleSidebar;
 
     public IStatusBarService getStatusBarService() {
         return mBarService;
@@ -1448,6 +1451,42 @@ public abstract class BaseStatusBar extends SystemUI implements
         }
         mContext.unregisterReceiver(mBroadcastReceiver);
     }
+
+     protected void addAppCircleSidebar() {
+         if (mAppCircleSidebar == null) {
+             mAppCircleSidebar = (AppCircleSidebar) View.inflate(mContext, R.layout.app_circle_sidebar, null);
+             mWindowManager.addView(mAppCircleSidebar, getAppCircleSidebarLayoutParams());
+         }
+     }
+ 
+     protected void removeAppCircleSidebar() {
+         if (mAppCircleSidebar != null) {
+             mWindowManager.removeView(mAppCircleSidebar);
+         }
+     }
+ 
+     protected WindowManager.LayoutParams getAppCircleSidebarLayoutParams() {
+         int maxWidth =
+                 mContext.getResources().getDimensionPixelSize(R.dimen.app_sidebar_trigger_width);
+ 
+         WindowManager.LayoutParams lp = new WindowManager.LayoutParams(
+                 maxWidth,
+                 ViewGroup.LayoutParams.MATCH_PARENT,
+                 WindowManager.LayoutParams.TYPE_STATUS_BAR_SUB_PANEL,
+                 0
+                 | WindowManager.LayoutParams.FLAG_TOUCHABLE_WHEN_WAKING
+                 | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                 | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                 | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
+                 | WindowManager.LayoutParams.FLAG_SPLIT_TOUCH,
+                 PixelFormat.TRANSLUCENT);
+         lp.privateFlags |= WindowManager.LayoutParams.PRIVATE_FLAG_NO_MOVE_ANIMATION;
+         lp.gravity = Gravity.TOP | Gravity.RIGHT;
+         lp.setTitle("AppCircleSidebar");
+ 
+         return lp;
+     }
+ 
 
     protected void setIconHiddenByUser(String iconPackage, boolean hide) {
         if (iconPackage == null
