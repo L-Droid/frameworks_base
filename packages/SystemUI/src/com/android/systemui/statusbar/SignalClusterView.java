565f81767311af1fe515f27a981d73945421c7f5
Francesco Colella <mnemonyc@hotmail.com>
2014-04-01 01:01:43 +0200

    Build fix for Chameleon Engine
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    
    Change-Id: If8066ac392ea43d230a8550c9e731bd2a6c49439

--- packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
@@ -104,11 +104,9 @@ public class SignalClusterView
         apply();
        
         mStatusBar.addIcon(mWifi);
- 		mStatusBar.addIcon(mMobile);
- 		mStatusBar.addIcon(mWifiActivity);
- 		mStatusBar.addIcon(mMobileActivity); 
- 		mStatusBar.addIcon(mMobileType); 
- 		mStatusBar.addIcon(mAirplane);
+		mStatusBar.addIcon(mMobile);
+		mStatusBar.addIcon(mMobileType); 
+		mStatusBar.addIcon(mAirplane);
     }
 
     @Override
