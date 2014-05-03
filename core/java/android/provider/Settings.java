f5fafc082f8862ff322dc99f22fc7bce411d4155
Janson Kang <temasek71@gmail.com>
2014-05-03 12:02:42 +0800

    Revert "frameworks: move keyguard wallpaper to system  (1/2)"
    This reverts commit 2e85d93d684e56f1e82128ae63b4053280b646e6.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -56,6 +56,7 @@ import android.text.TextUtils;
 import android.util.AndroidException;
 import android.util.Log;
 
+import com.android.internal.util.cm.LockscreenBackgroundUtil;
 import com.android.internal.widget.ILockSettings;
 
 import java.net.URISyntaxException;
