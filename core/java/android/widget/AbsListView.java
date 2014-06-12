5cd6ca1c36472e162f7190e3ea74bd3fe57941d6
Jorge Ruesga <jorge@ruesga.com>
2014-06-12 02:25:28 +0200

    listview: ensure active views are filled prior than scrap views
    The internal Recycler#fillActiveViews should be called prior to add any view
    to the scrapViews queue, so it can be use safely without detaching views
    because out of sync.
    
    Change-Id: Ie119ad871a89e0a3fa1e07e955fcd5ca7b171f46
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- core/java/android/widget/AbsListView.java
+++ core/java/android/widget/AbsListView.java
@@ -6687,6 +6687,12 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
         private SparseArray<View> mTransientStateViews;
         private LongSparseArray<View> mTransientStateViewsById;
 
+        private boolean mIsActiveViewsInitialized;
+
+        boolean isActiveViewsInitialized() {
+            return mIsActiveViewsInitialized;
+        }
+
         public void setViewTypeCount(int viewTypeCount) {
             if (viewTypeCount < 1) {
                 throw new IllegalArgumentException("Can't have a viewTypeCount < 1");
@@ -6758,6 +6764,9 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
             }
 
             clearTransientStateViews();
+
+            // We want to a refresh of mActiveViews
+            mIsActiveViewsInitialized = false;
         }
 
         /**
@@ -6768,6 +6777,11 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
          *        mActiveViews
          */
         void fillActiveViews(int childCount, int firstActivePosition) {
+            // The recyclebin is initialized when we have some active views.
+            if (childCount > 0) {
+                mIsActiveViewsInitialized = true;
+            }
+
             if (mActiveViews.length < childCount) {
                 mActiveViews = new View[childCount];
             }
