467de75fad21ec917322ef58068d0715de22e284
Der-Schubi <schubi@erlangen.ccc.de>
2014-06-16 18:56:48 -0300

    Hover settings (1/2)
    - Require full screen mode;
    - Hide non-clearable;
    - Exclude low priority;
    
    Original commits
    https://github.com/TheKang/frameworks_base/commit/598fa9d46e84b563d9ec6ffa75a38da9fc3cc5a9
    https://github.com/TheKang/frameworks_base/commit/a3b44ef4fce7cdabc477075ac646b596dc0f83a4
    
    Thanks @Der-Schubi
    
    Change-Id: If3c34496acf75d49434902c49dfe48f997c0b668
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4708,6 +4708,27 @@ public final class Settings {
         public static final String HOVER_STATE = "hover_state";
 
         /**
+         * Hover: Exclude non-clearable notifications, default is 0 (off).
+         *
+         * @hide
+         */
+        public static final String HOVER_EXCLUDE_NON_CLEARABLE = "hover_exclude_non_clearable";
+
+        /**
+         * Hover: Exclude low priority notifications, default is 0 (off).
+         *
+         * @hide
+         */
+        public static final String HOVER_EXCLUDE_LOW_PRIORITY = "hover_exclude_low_priority";
+
+        /**
+         * Hover: Only show up if StatusBar is hidden, default is 1 (on).
+         *
+         * @hide
+         */
+        public static final String HOVER_REQUIRE_FULLSCREEN_MODE = "hover_require_fullscreen_mode";
+
+        /**
          * Recent panel: Show topmost task
          *
          * @hide
