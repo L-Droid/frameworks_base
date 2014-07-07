702ab34784329e522f5e7081dc767e40002bc7b3
doc <doc.divxm@gmail.com>
2014-07-07 15:45:15 +0300

    FWB: Fix FC on Floating window after tap outside
    Speacial thanx to @pappschlumpf from XDA for derp hunting :)
    
    Change-Id: I43e65d1c77d0e25dd8ee8e521e2f839612d16883
    Signed-off-by: doc <doc.divxm@gmail.com>

--- core/java/android/app/Activity.java
+++ core/java/android/app/Activity.java
@@ -6006,7 +6006,7 @@ public class Activity extends ContextThemeWrapper
             mWindow.mIsFloatingWindow = true;
             if (!isAlreadyAttachToWindow) {
                 isAlreadyAttachToWindow = true;
-                mWindow.setCloseOnTouchOutsideIfNotSet(true);
+                mWindow.setCloseOnTouchOutsideIfNotSet(false);
                 mWindow.setGravity(Gravity.CENTER);
                 // Scale it
                 scaleFloatingWindow();
