fcb1933d436ce0450bc63d28d409a862f9b0ede8
Chris Craik <ccraik@google.com>
2014-07-22 12:30:39 -0700

    Restore untrusted glStencilMask value on resume after functor
    Fixes an issue where the stencil buffer is corrupted
    (used in overdraw debugging, path clipping) when used in
    conjunction with a HW accelerated WebView.
    
    bug:15023700
    bug:16465551
    
    Change-Id: If201b503931380f7107a34e9a9a2f877a50f72b1

--- libs/hwui/OpenGLRenderer.cpp
+++ libs/hwui/OpenGLRenderer.cpp
@@ -399,6 +399,8 @@ void OpenGLRenderer::resume() {
     glEnable(GL_BLEND);
     glBlendFunc(mCaches.lastSrcMode, mCaches.lastDstMode);
     glBlendEquation(GL_FUNC_ADD);
+
+    glStencilMask(0xff);
 }
 
 void OpenGLRenderer::resumeAfterLayer() {
