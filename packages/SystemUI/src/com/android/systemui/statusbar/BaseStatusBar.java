873a5893fb0ebb1f1ece9bf30af2cb9c5440f437
lion0738 <lion0738@naver.com>
2014-07-31 20:06:54 -0300

    Launch Heads Up in floating window [1/2]     AICP'fied.     To make floating window to work for Hover and Heads Up, notificationhelper     interface is modified to include a third parameter to indicate whether it's     called from hover or not.     To make Heads_Up floating notification work separate from Hover,     a third parameter is added to the notificationhelper to indicate whether     we are called from Hover.java. This changes the call.     PS2-3: Fix authorship     PS5: separate hover change.     PS6: change commit message.     PS7: Merge with Hover change, fix setting preference and code cleanup.          To make settings work, don't use Boolean with CM CheckBox preference     PS8: autorship

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -1685,7 +1685,7 @@ public abstract class BaseStatusBar extends SystemUI implements
                     } else {
                         if (DEBUG) Log.d(TAG, "updating the current heads up:" + notification);
                         mInterruptingNotificationEntry.notification = notification;
-                        updateNotificationViews(mInterruptingNotificationEntry, notification);
+                        updateNotificationViews(mInterruptingNotificationEntry, notification, true);
                     }
                 } else if (shouldInterrupt(notification)
                         && panelsEnabled() && !isHeadsUpInSnooze() && mShowHeadsUpUpdates) {
@@ -1751,6 +1751,11 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     private void updateNotificationViews(NotificationData.Entry entry,
             StatusBarNotification notification) {
+        updateNotificationViews(entry, notification, false);
+    }
+
+    private void updateNotificationViews(NotificationData.Entry entry,
+            StatusBarNotification notification, boolean headsUp) {
         final RemoteViews contentView = notification.getNotification().contentView;
         final RemoteViews bigContentView = notification.getNotification().bigContentView;
         // Reapply the RemoteViews
@@ -1761,8 +1766,8 @@ public abstract class BaseStatusBar extends SystemUI implements
         // update the contentIntent
         final PendingIntent contentIntent = notification.getNotification().contentIntent;
         if (contentIntent != null) {
-            final View.OnClickListener listener = makeClicker(contentIntent,
-                    notification.getPackageName(), notification.getTag(), notification.getId());
+            final View.OnClickListener listener =
+                    mNotificationHelper.getNotificationClickListener(entry, headsUp, false);
             entry.content.setOnClickListener(listener);
             entry.floatingIntent = makeClicker(contentIntent,
                     notification.getPackageName(), notification.getTag(), notification.getId());
