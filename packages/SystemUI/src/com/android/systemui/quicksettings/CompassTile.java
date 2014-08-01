2f5748337e3973d87e474b03a8c2de0f877dd09f
Nebojsa Cvetkovic <nebkat@gmail.com>
2014-08-01 15:28:12 +0100

    SystemUI: Replace CompassTile RotateAnimation with tweening
    Change-Id: I53b2155232122ae5dbed611db3865e0ffdbde37e
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/quicksettings/CompassTile.java

--- packages/SystemUI/src/com/android/systemui/quicksettings/CompassTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/CompassTile.java
@@ -17,41 +17,51 @@
 package com.android.systemui.quicksettings;
 
 import android.content.Context;
-import android.graphics.Matrix;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.view.animation.Animation;
-import android.view.animation.RotateAnimation;
+import android.os.Handler;
+import android.os.Message;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.ImageView.ScaleType;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.android.systemui.R;
+import com.android.systemui.statusbar.phone.QuickSettingsContainerView;
 import com.android.systemui.statusbar.phone.QuickSettingsController;
 import com.android.systemui.statusbar.phone.QuickSettingsTileView;
 
-public class CompassTile extends QuickSettingsTile implements SensorEventListener {
+public class CompassTile extends QuickSettingsTile implements
+        SensorEventListener, View.OnClickListener, QuickSettingsTileView.OnPrepareListener {
     private boolean mActive = false;
-    private boolean mWasActive = false;
-    private float mCurrentDegree = 0;
+    private boolean mPrepared = false;
 
-    private final static float DEFAULT_IMAGE_ROTATION = 90;
-    private final static int ANIMATION_DURATION = 200;
+    private float mDegree = 0f;
+
     private final static float ALPHA = 0.97f;
+    private final static int MSG_UPDATE_COMPASS = 1;
+    private final static int COMPASS_TILE_UPDATE_INTERVAL = 16;
 
     private SensorManager mSensorManager;
     private Sensor mAccelerationSensor;
     private Sensor mGeomagneticFieldSensor;
 
-    private float[] mAcceleration = new float[3];
-    private float[] mGeomagnetic = new float[3];
+    private float[] mAcceleration;
+    private float[] mGeomagnetic;
 
     private TextView mText;
     private ImageView mImage;
+
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == MSG_UPDATE_COMPASS) {
+                updateCompassTile();
+            }
+        }
+    };
 
     public CompassTile(Context context, QuickSettingsController qsc) {
         super(context, qsc);
@@ -60,14 +70,15 @@ public class CompassTile extends QuickSettingsTile implements SensorEventListene
         mSensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
         mAccelerationSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mGeomagneticFieldSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+    }
 
-        mOnClick = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mActive = !mActive;
-                updateResources();
-            }
-        };
+    @Override
+    public void setupQuickSettingsTile(LayoutInflater inflater,
+            QuickSettingsContainerView container) {
+        super.setupQuickSettingsTile(inflater, container);
+
+        // Override click listener to prevent collapsing the panel on click
+        mTile.setOnClickListener(this);
     }
 
     @Override
@@ -79,25 +90,23 @@ public class CompassTile extends QuickSettingsTile implements SensorEventListene
         updateTile();
         super.onPostCreate();
 
-        mTile.setOnPrepareListener(new QuickSettingsTileView.OnPrepareListener() {
-            @Override
-            public void onPrepare() {
-                if (mWasActive) {
-                    mActive = true;
-                    mWasActive = false;
-                    updateResources();
-                }
-            }
+        mTile.setOnPrepareListener(this);
+    }
 
-            @Override
-            public void onUnprepare() {
-                if (mActive) {
-                    mActive = false;
-                    mWasActive = true;
-                    updateResources();
-                }
-            }
-        });
+    @Override
+    public void onPrepare() {
+        mPrepared = true;
+        if (mActive) {
+            updateResources();
+        }
+    }
+
+    @Override
+    public void onUnprepare() {
+        mPrepared = false;
+        if (mActive) {
+            updateResources();
+        }
     }
 
     @Override
@@ -106,11 +115,16 @@ public class CompassTile extends QuickSettingsTile implements SensorEventListene
         super.updateResources();
     }
 
+    @Override
+    public void onClick(View v) {
+        mActive = !mActive;
+        updateResources();
+    }
+
     private synchronized void updateTile() {
-        if (mActive) {
+        if (mActive && mPrepared) {
             mDrawable = R.drawable.ic_qs_compass_on;
-            mLabel = "270° " + mContext.getResources().getStringArray(
-                    R.array.cardinal_directions)[6];
+            mLabel = mContext.getString(R.string.quick_settings_compass_init);
 
             // Register listeners
             mSensorManager.registerListener(
@@ -122,75 +136,81 @@ public class CompassTile extends QuickSettingsTile implements SensorEventListene
             mLabel = mContext.getString(R.string.quick_settings_compass_off);
 
             // Reset rotation of the ImageView
-            RotateAnimation rotateAnimation = new RotateAnimation(mCurrentDegree, 0,
-                    Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-            rotateAnimation.setDuration(0);
-            rotateAnimation.setFillAfter(true);
-            mImage.startAnimation(rotateAnimation);
+            mImage.setRotation(0);
 
             // Remove listeners
             mSensorManager.unregisterListener(this);
+            mHandler.removeMessages(MSG_UPDATE_COMPASS);
         }
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        synchronized (this) {
-            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-                mAcceleration[0] = ALPHA * mAcceleration[0] + (1 - ALPHA) * event.values[0];
-                mAcceleration[1] = ALPHA * mAcceleration[1] + (1 - ALPHA) * event.values[1];
-                mAcceleration[2] = ALPHA * mAcceleration[2] + (1 - ALPHA) * event.values[2];
+        float[] values;
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            if (mAcceleration == null) {
+                mAcceleration = event.values.clone();
             }
 
-            if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
-                mGeomagnetic[0] = ALPHA * mGeomagnetic[0] + (1 - ALPHA) * event.values[0];
-                mGeomagnetic[1] = ALPHA * mGeomagnetic[1] + (1 - ALPHA) * event.values[1];
-                mGeomagnetic[2] = ALPHA * mGeomagnetic[2] + (1 - ALPHA) * event.values[2];
+            values = mAcceleration;
+        } else {
+            // Magnetic field sensor
+            if (mGeomagnetic == null) {
+                mGeomagnetic = event.values.clone();
             }
 
-            if (mActive && mAcceleration != null && mGeomagnetic != null) {
-                float R[] = new float[9];
-                float I[] = new float[9];
-                boolean success = SensorManager.getRotationMatrix(
-                        R, I, mAcceleration, mGeomagnetic);
-
-                if (success) {
-                    float orientation[] = new float[3];
-                    SensorManager.getOrientation(R, orientation);
-
-                    // Convert azimuth zu degree
-                    float degree = (float) Math.toDegrees(orientation[0]);
-                    degree = (degree + 360) % 360;
-
-                    // Set rotation in degrees as tile title
-                    mText.setText(String.format("%s", Math.round(degree))
-                            + "° " + getCardinalDirection(degree));
-
-                    // Take the default rotation of the compass image into account
-                    degree += DEFAULT_IMAGE_ROTATION;
-
-                    // Create a rotation animation
-                    RotateAnimation rotateAnimation = new RotateAnimation(mCurrentDegree, -degree,
-                            Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-
-                    // Set animation duration
-                    rotateAnimation.setDuration(ANIMATION_DURATION);
-                    rotateAnimation.setFillAfter(true);
-
-                    // Start the animation and update mCurrentDegree
-                    mImage.startAnimation(rotateAnimation);
-                    mCurrentDegree = -degree;
-                }
-            }
+            values = mGeomagnetic;
         }
+
+        for (int i = 0; i < 3; i++) {
+            values[i] = ALPHA * values[i] + (1 - ALPHA) * event.values[i];
+        }
+
+        if (!mActive || !mPrepared || mAcceleration == null || mGeomagnetic == null) {
+            // Nothing to do at this moment
+            return;
+        }
+
+        float R[] = new float[9];
+        float I[] = new float[9];
+        if (!SensorManager.getRotationMatrix(R, I, mAcceleration, mGeomagnetic)) {
+            // Rotation matrix couldn't be calculated
+            return;
+        }
+
+        // Get the current orientation
+        float[] orientation = new float[3];
+        SensorManager.getOrientation(R, orientation);
+
+        // Convert azimuth to degrees
+        float newDegree = (float) Math.toDegrees(orientation[0]);
+        newDegree = (newDegree + 360) % 360;
+        if (!mHandler.hasMessages(MSG_UPDATE_COMPASS)) {
+            mHandler.sendEmptyMessageDelayed(MSG_UPDATE_COMPASS, COMPASS_TILE_UPDATE_INTERVAL);
+        }
+
+        mDegree = newDegree;
     }
 
-    private String getCardinalDirection(float degree) {
+    private void updateCompassTile() {
+        // Set rotation in degrees as tile title
+        mText.setText(formatValueWithCardinalDirection(mDegree));
+
+        float target = 360 - mDegree;
+
+        float relative = target - mImage.getRotation();
+        if (relative > 180) relative -= 360;
+
+        mImage.setRotation(mImage.getRotation() + relative / 2);
+    }
+
+    private String formatValueWithCardinalDirection(float degree) {
         int cardinalDirectionIndex = (int) (Math.floor(((degree - 22.5) % 360) / 45) + 1) % 8;
         String[] cardinalDirections = mContext.getResources().getStringArray(
                 R.array.cardinal_directions);
 
-        return cardinalDirections[cardinalDirectionIndex];
+        return mContext.getString(R.string.quick_settings_compass_value, degree,
+                cardinalDirections[cardinalDirectionIndex]);
     }
 
     @Override
