X-ROM/android_frameworks_base

fb: TRDS 3.0 and new theme engine preparation (1/4)
For the one who do not know this feature see as reference
the first two versions here:

SlimRoms/frameworks_base@b330fde

SlimRoms/frameworks_base@3194015

we got now over a year now a lot experience how to handle the qualifier fallback
as well how to theme it and how to handle other apps. Based on this knowledge we
redid some parts of TRDS engine with following new features/additions and fixes
to have now a final perfect working TRDS

- compared to the old versions where it was needed on some apps to restart them
  in background this is not needed any more. All is handled about the internal
  AOSP resource handling and on qualifier change the caches are cleared
  and the apps get an onconfigurationchanged to restart the activity
  like it is doing on orientation change eg
- inverted folder is now depreciated and the new folders are holodark
  and hololight which allows on the one hand a dark theme (at least
  how TRDS started) and a light based theme hololight
- This name change is mainly because of the new theme engine which
  will come which uses this as base to add 2 external themes at the same
  time and give the user the ability to switch between them. But this is
  still in early stages and this commit is just the preparation for it
- Two new automodes are now integrated. User can enable the twilight modus
  which enables/disables holodark/hololight depending on the daytime...means
  on the evening it switches to holodark on normal day it is hololight.
  The other automode is based on light conditions....means that if it is
  dark outside it switches to holodark....if we have bright light conditions
  it switches immediatelly to hololight. Due of the resource cache handling now
  this happens immediatelly for the user and feels completly system integrated
  and happens without delay. A bit comparable to some navigation tool for cars
  which can switch automatically from dark to light and back
- currently the

core/java/com/android/internal/util/slim/TRDSConstant.java
@@ -1,0 +1,7 @@
+package com.android.internal.util.slim;
+
+// Using this very simple class to not mixe up with CyanogenMod stuffs
+// Just to keep it it esay to cherry-pick 
+public class TRDSConstant {
+    public static final String ACTION_THEME_SWITCH         = "**theme_switch**";
+}

L-Droid/frameworks_base

Conflicts:
	core/res/res/values/cr_config.xml
	core/res/res/values/cr_symbols.xml
	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

core/java/com/android/internal/util/crdroid/TRDSConstant.java
@@ -1,0 +1,7 @@
+package com.android.internal.util.crdroid;
+
+// Using this very simple class to not mixe up with CyanogenMod stuffs
+// Just to keep it it esay to cherry-pick 
+public class TRDSConstant {
+    public static final String ACTION_THEME_SWITCH         = "**theme_switch**";
+}