b2ad23663388141f5aae4ce74ceaacd84e1cd5b5
SeniorLimpio <seniorlimpio1@gmail.com>
2014-08-31 21:06:42 +1200

    Fix build

--- core/java/android/view/ViewConfiguration.java
+++ core/java/android/view/ViewConfiguration.java
@@ -239,7 +239,7 @@ public class ViewConfiguration {
     /**
      * Maximum velocity to initiate a fling, as measured in dips per second
      */
-    private static final int MAXIMUM_FLING_VELOCITY = 15000;
+    private static int MAXIMUM_FLING_VELOCITY = 15000;
 
     /**
      * Maximum velocity to initiate a fling, as measured in dips per second
@@ -265,7 +265,7 @@ public class ViewConfiguration {
     /**
      * The coefficient of friction applied to flings/scrolls.
      */
-    private static final float SCROLL_FRICTION = 0.011f;
+    private static float SCROLL_FRICTION = 0.009f;
 
     /**
      * The coefficient of friction applied to flings/scrolls.
